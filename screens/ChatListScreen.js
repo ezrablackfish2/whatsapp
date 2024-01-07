ea098a27b5c1f5099bc2f26bc6198130fd444e58
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-01-07 10:30:11 +0300

    nice progress

--- screens/ChatListScreen.js
+++ screens/ChatListScreen.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const ChatListScreen = props => {
     
     return (
 
-	<View>
+	<View style={styles.container}>
         <Text >Chat list screen</Text>
     </View>
 
     );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    fontFamily: "Bellota",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+	label : {
+		fontFamily: "Bellota",
+		fontSize: 20,
+	},
+});
 
 export default ChatListScreen;
