534a640f3f7f599f18bd1cccf9e4888e0b4db1f5
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-01-07 10:22:19 +0300

    on layout

--- App.js
+++ App.js
@@ -58,7 +58,7 @@ export default function App() {
   }
 
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider onLayout={onLayout}>
         <NavigationContainer>
           <Stack.Navigator>
             <Stack.Screen name="Home" component={ChatListScreen} />
