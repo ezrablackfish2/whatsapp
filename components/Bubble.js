97bd1632c18e59b5c0c1a73c49c2defad50ac84c
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-01-25 17:23:56 +0300

    wow wow wow

---
+++ components/Bubble.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import colors from "../constants/colors";
+
+const Bubble = props => {
+	const { text, type } = props;
+
+	const bubbleStyle = {  ...styles.container };
+	const textStyle = { ...styles.text };
+
+	switch (type) {
+		case "system":
+			textStyle.color = "#656448";
+			bubbleStyle.backgroundColor = colors.beige;
+			bubbleStyle.alignItems = "center";
+			bubbleStyle.marginTop = 10;
+			break;
+
+		default:
+			break;
+	}
+
+	return (
+		<View style={styles.wrapperStyle}>
+			<View style={bubbleStyle}>
+				<Text style={textStyle}>
+					{text}
+				</Text>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	wrapperStyle: {
+		flexDirection: "row",
+		justifyContent: "center",
+	},
+	container: {
+		backgroundColor: "white",
+		borderRadius: 6,
+		padding: 5,
+		marginBottom: 10,
+		borderColor: "#E2DACC",
+		borderWidth: 1,
+
+
+	},
+
+	text: {
+		fontFamily: "Chakra",
+		letterSpacing: 0.3,
+	},
+})
+
+export default Bubble;
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -5,6 +5,9 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Feather } from '@expo/vector-icons';
 import colors from "../constants/colors.js";
 import { useSelector, useDispatch } from "react-redux";
+import PageContainer from "../components/PageContainer"
+import Bubble from "../components/Bubble";
+import { createChat } from "../utils/actions/chatActions";
 
 
 const ChatScreen = props => {
@@ -14,6 +17,7 @@ const ChatScreen = props => {
   
 	const [chatUsers, setChatUsers] = useState([]);
 	const [messageText, setMessageText] = useState("");
+	const [chatId, setChatId] = useState(props.route?.params?.chatId);
 
 	const chatData = props.route?.params?.newChatData;
 
@@ -33,9 +37,25 @@ const ChatScreen = props => {
 		setChatUsers(chatData.users);
 	}, [chatUsers])
 
-	const sendMessage = useCallback( () => {
+	const sendMessage = useCallback( async () => {
+
+
+		try {
+			let id = chatId;
+			if (!id) {
+				id = await createChat(userData.userId, props.route.params.newChatData);
+				setChatId(id);
+			}
+
+
+
+		} catch (error) {
+
+		}
+
+
 		setMessageText("");	
-	}, [messageText]);
+	}, [messageText, chatId, createChat, userData.userId, props.route.params.newChatData]);
 
     return (
 	    <SafeAreaView 
@@ -51,6 +71,14 @@ const ChatScreen = props => {
 
 
 		<ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+
+	    <PageContainer style={{ backgroundColor: "transparent" }}>
+	
+		{
+			!chatId && <Bubble text="This is a new chat. Say Hi" type="system" />	
+		}
+
+	    </PageContainer>
 
 	    	</ImageBackground>
 	    <View style={styles.inputContainer}>
