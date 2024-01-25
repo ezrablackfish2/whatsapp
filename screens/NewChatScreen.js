c870f1ce91bd076dd93e980e68b71c5738072a2a
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-01-25 10:19:29 +0300

    things are going good

--- screens/NewChatScreen.js
+++ screens/NewChatScreen.js
@@ -1,9 +1,19 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 import { HeaderButtons, Item  } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/CustomHeaderButton";
+import PageContainer from "../components/PageContainer";
+import { FontAwesome } from '@expo/vector-icons';
+import colors from "../constants/colors";
+import commonStyles from "../constants/commonStyles";
+
 
 const NewChatScreen = props => {
+
+
+		const [isLoading, setIsLoading] = useState(false);
+		const [users, setUsers] = useState();
+		const [noResultsFound, setNoResultsFound] = useState(false);
     
 
 	    useEffect(() => {
@@ -23,26 +33,79 @@ const NewChatScreen = props => {
 	}, []);
     
 	return (
+		<PageContainer>
+			<View style={styles.searchContainer}>
+			<FontAwesome name="search" size={15} color={colors.lightGrey} />
 
-	<View style={styles.container}>
-        <Text >New Chat Screen</Text>
+			<TextInput 
+				placeholder="Search"
+				style={styles.searchBox}
+				onChangeText={() => {}}
 
-    </View>
+			/> 
+			</View>
+
+
+			{
+				!isLoading && noResultsFound && (
+					<View style={commonStyles.center}>
+						<FontAwesome 
+						name="question" 
+						size={55} 
+						color={colors.lightGrey} 
+						style={styles.noResultsIcon}/>
+						<Text style={styles.noResultsText}>
+					No Users Found!
+						</Text>
+					</View>
+				)	
+			}
+
+			{
+				!isLoading && !users && (
+					<View style={commonStyles.center}>
+						<FontAwesome 
+						name="users" 
+						size={55} 
+						color={colors.lightGrey} 
+						style={styles.noResultsIcon}/>
+						<Text style={styles.noResultsText}>
+					Enter a name to search for a user!
+						</Text>
+					</View>
+				)	
+			}	
+
+
+		</PageContainer>
     );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    fontFamily: "Bellota",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-	label : {
-		fontFamily: "Bellota",
-		fontSize: 20,
+	searchContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		backgroundColor: colors.extraLightGrey,
+		height: 30,
+		marginVertical: 8,
+		paddingHorizontal: 8,
+		paddingVertical: 5,
+		borderRadius: 5,
 	},
+	searchBox: {
+		marginLeft: 8,
+		fontSize: 15,
+		width: "100%",
+		fontFamily: "Croissant",
+	},
+	noResultsIcon: {
+		marginBottom: 20,		
+	},
+	noResultsText: {
+		color: colors.textColor,
+		fontFamily: "Croissant"
+	},
+
 });
 
 export default NewChatScreen;
